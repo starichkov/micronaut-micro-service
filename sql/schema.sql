1656d8e7b13924a17a48ff1904bd9d9d7938681a
Vadim Starichkov <starichkovva@gmail.com>
2025-02-01 18:15:39 +0200

    #1 - WIP simple notes service

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,8 +1,8 @@
-create database if not exists microshop character set utf8mb4;
+create database if not exists micro_notes character set utf8mb4;
 
-use microshop;
+use micro_notes;
 
-create user if not exists patrician;
-alter user patrician@'%' identified by '';
-grant insert, select, delete, update, references on microshop.* to patrician@'%';
-grant create on microshop.* to patrician@'%';
+create user if not exists author;
+alter user author@'%' identified by '';
+grant insert, select, delete, update, references on micro_notes.* to author@'%';
+grant create on micro_notes.* to author@'%';
